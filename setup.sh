4cba24202ae0e4fb57349dfdf60d288d65514729
Timothy Weber <tw@timothyweber.org>
2018-08-22 17:30:59 -0400

    Instsall Sublime Text and link its settings here.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 # Set up my dotfiles initially.
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+
+# Install Sublime Text
+cd ~/Downloads
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+cd ~
+
+if [ -d ~/.config/sublime-text-3/Packages/User ]; then
+	mv ~/.config/sublime-text-3/Packages/User ~/.config/sublime-text-3/Packages/User-orig
+fi
+ln -s ~/dotfiles/SublimeUser ~/.config/sublime-text-3/Packages/User
