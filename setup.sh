80402f598bad0dfdacb3ccd1d92c59a3f753894d
Timothy Weber <tw@6sys.net>
2018-08-24 13:32:19 -0400

    Load submodule so bashgit works.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 # Configure Git to cache my credentials temporarily.
 git config credential.helper 'cache --timeout=1200'
 
+# Load the submodule(s) for this repo.
+git submodule init
+git submodule update
+
 # Install Sublime Text
 cd ~/Downloads
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
