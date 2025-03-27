2c4fba67209e7dd37a986cb11c4133c7eea06ae5
Timothy Weber <tw@timothyweber.org>
2025-03-27 12:36:16 -0400

    Added dev-basics.sh to install Python development basics.

---
+++ dev-basics.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install prereqs for building Python.
+sudo apt-get install -y \
+  make \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  wget \
+  curl \
+  llvm \
+  libncurses5-dev \
+  libncursesw5-dev \
+  xz-utils \
+  tk-dev \
+  liblzma-dev \
+  || exit
+
+# Install pyenv.
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+exec "$SHELL"
+
+echo Now you may need to exit and return.
+echo Under WSL, do `wsl --shutdown` and then `wsl` again.
+
